ea56be4e078d6b026735802b3276a534cc76773f
Rohit <rohitmallapur612@gmail.com>
2026-01-31 20:48:05 +0530

    Add JPA repository for Employee entity

--- src/main/java/Spring/API/EMS_Project/repository/EMSRepository.java
+++ src/main/java/Spring/API/EMS_Project/repository/EMSRepository.java
@@ -1,5 +1,8 @@
 package Spring.API.EMS_Project.repository;
 
-public interface EMSRepository {
+import Spring.API.EMS_Project.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EMSRepository extends JpaRepository<Employee, Long> {
 
 }
