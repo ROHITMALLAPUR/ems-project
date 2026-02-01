f3c9c16d09cf5dcdf2ccdac4c2bd5d698fb97133
Rohit <rohitmallapur612@gmail.com>
2026-02-01 19:49:06 +0530

    added ResponseEntity for the CRUD Controllers and have returned with HttpStatus message

--- src/main/java/Spring/API/EMS_Project/controller/EMSController.java
+++ src/main/java/Spring/API/EMS_Project/controller/EMSController.java
@@ -5,10 +5,12 @@ package Spring.API.EMS_Project.controller;
 import Spring.API.EMS_Project.entity.Employee;
 import Spring.API.EMS_Project.services.EMSServices;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/Employee_Details")
@@ -19,32 +21,43 @@ public class EMSController {
 
 
     @PostMapping
-    public boolean createEmployee(@RequestBody Employee employeeEntry) {
-        emsServices.saveEmployee(employeeEntry);
-        return true;
+    public ResponseEntity<Employee> createEmployee(@RequestBody Employee employeeEntry) {
+        try{emsServices.saveEmployee(employeeEntry);
+            return new ResponseEntity<>(employeeEntry,HttpStatus.CREATED);
+        }
+        catch(Exception e){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
     }
 
     @GetMapping
-    public List<Employee> getAll() {
-        return emsServices.getAll();
+    public ResponseEntity<?> getAll() {
+        List<Employee> allEmpList= emsServices.getAll();
+        if(allEmpList!=null && !allEmpList.isEmpty()){
+            return new ResponseEntity<>(allEmpList,HttpStatus.OK);
+        }
+        else{
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
 
     @GetMapping({"id/{myId}"})
-    public Employee getEmployeeById(@PathVariable Long myId) {
-        return  emsServices.getById(myId).orElse(null);
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable Long myId) {
+       Optional<Employee> emp= emsServices.getById(myId);
+        return emp.map(employee -> new ResponseEntity<>(employee, HttpStatus.OK)).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
     @DeleteMapping({"id/{myId}"})
-    public boolean deleteEmployeeById(@PathVariable Long myId) {
+    public ResponseEntity<?> deleteEmployeeById(@PathVariable Long myId) {
         emsServices.deleteById(myId);
-        return true;
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @PutMapping({"id/{myId}"})
     public ResponseEntity<Employee> updateEmployeeById(@PathVariable Long myId, @RequestBody Employee employeeEntry) {
         Employee updatedEmployee=emsServices.updateById(myId,employeeEntry);
-        return ResponseEntity.ok(updatedEmployee);
+        return new ResponseEntity<>(updatedEmployee,HttpStatus.OK);
     }
 }
 
