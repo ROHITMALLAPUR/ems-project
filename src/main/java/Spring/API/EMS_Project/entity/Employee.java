ccced4444d71274262cd38aab03a589788cddedf
Rohit <rohitmallapur612@gmail.com>
2026-02-01 20:59:07 +0530

    Integrated Lombok to reduce boilarplate code used lombok in getters and setters in Employee Entity

--- src/main/java/Spring/API/EMS_Project/entity/Employee.java
+++ src/main/java/Spring/API/EMS_Project/entity/Employee.java
@@ -5,10 +5,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDate;
 
 @Entity
+@Getter
+@Setter
 public class Employee {
 
     @Id
@@ -22,67 +27,4 @@ public class Employee {
     private Role role;
     private Status status;
 
-    public long getId() {
-        return this.id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDepartment() {
-        return this.department;
-    }
-
-    public void setDepartment(String department) {
-        this.department = department;
-    }
-
-    public String getEmail() {
-        return this.email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public long getSalary() {
-        return this.salary;
-    }
-
-    public void setSalary(long salary) {
-        this.salary = salary;
-    }
-
-    public LocalDate getDateofJoining() {
-        return this.dateofJoining;
-    }
-
-    public void setDateofJoining(LocalDate dateofJoining) {
-        this.dateofJoining = dateofJoining;
-    }
-
-    public Role getRole() {
-        return this.role;
-    }
-
-    public void setRole(Role role) {
-        this.role = role;
-    }
-
-    public Status getStatus() {
-        return this.status;
-    }
-
-    public void setStatus(Status status) {
-        this.status = status;
-    }
 }
